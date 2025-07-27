356ec4fc435d6fc0cea6f17b2a34c7ecce820ba2
PiotrFLEURY <piotr.fleury@gmail.com>
2025-07-27 16:52:32 +0200

    fix(timezone): display local time

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:tv_program/views/tv_program.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    const ProviderScope(
-      child: TvProgram(),
-    ),
-  );
+  initializeDateFormatting('fr_FR', null).then((_) {
+    runApp(const ProviderScope(child: TvProgram()));
+  });
 }
