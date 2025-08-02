6c282eafbed48dcbdb942992ffff52bf483d482f
PiotrFLEURY <piotr.fleury@gmail.com>
2025-08-02 09:28:30 +0200

    feat(about): add about sections

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:tv_program/views/tv_program.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
   initializeDateFormatting('fr_FR', null).then((_) {
-    runApp(const ProviderScope(child: TvProgram()));
+    runApp(
+      ProviderScope(
+        child: TvProgram(
+          packageInfo: packageInfo,
+        ),
+      ),
+    );
   });
 }
